c70763e96481b7bf9a08ae7f918f93573c12d621
yorxcontreras <contrerasyorx@gmail.com>
2024-08-28 09:36:06 -0300

    vercion 1

---
+++ exerB/Program.cs
@@ -0,0 +1,39 @@
+﻿int tamanho = 100;
+int[] vetor = new int[tamanho];
+Random random = new Random();
+for (int i = 0 ; i < vetor.Length; i++){
+
+vetor[i]  = random.Next(1000);
+
+}
+
+for (int i = 0 ; i < vetor.Length; i++)
+{
+Console.Write(vetor[i] + " ");
+}
+
+bool troca = false;
+
+do{
+    
+    troca = false;
+
+for (int i = 0 ; i < vetor.Length - 1; i++)
+{
+     
+    if (vetor[i] > vetor[i+1])
+        {
+            troca = true;
+            int aux = vetor[i];
+            vetor[i]=vetor[i+1];
+            vetor[i+1]= aux;
+        }
+}
+}while(troca == true);
+
+Console.WriteLine("\n");
+
+for (int i = 0 ; i < vetor.Length; i++)
+{
+Console.Write(vetor[i] + " ");
+}
