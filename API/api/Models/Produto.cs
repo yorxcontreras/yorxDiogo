6f01a1c1855e766e2c68d428e8ff679117671c51
yorxcontreras <contrerasyorx@gmail.com>
2024-09-04 11:30:28 -0300

    vercion 6

---
+++ API/api/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace api.Models;
+
+public class Produto
+{
+
+    public Produto()
+    {
+        Id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+    }
+
+    public string? Id {get; set;}
+    public string? Nome {get; set;}
+    public double Preco {get; set;}
+
+    public int Quantidade {get; set;}
+
+    public DateTime CriadoEm { get; set; }
+
+    // private double preco;
+
+    // public double getPreco()
+    // {
+    //     return preco;
+    // }
+    // public void setPreco(double preco){
+    //     this.preco = preco * 3;
+    // }
+}
--- API/api/Program.cs
+++ API/api/Program.cs
@@ -1,20 +1,64 @@
+using api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/",()=>"hello man");
+app.MapGet("/",()=>" API de Produto");
 
-app.MapGet("/segundafuncionalidade",()=>"segundo hello man");
-
-app.MapGet("/retornar",()=>
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto()
 {
-    dynamic endereco = new {
-        rua = "parigo de sousa",
-        numero = 5300,
-        cep = "81290-350"
-    };
-    return endereco;
+    Nome = "Notebook",
+    Preco = 2000,
+    Quantidade = 54
 });
+produtos.Add(new Produto()
+{
+    Nome = "pc",
+    Preco = 5000,
+    Quantidade = 23
+});produtos.Add(new Produto()
+{
+    Nome = "Grafic",
+    Preco = 800,
+    Quantidade = 78
+});
+produtos.Add(new Produto()
+{
+    Nome = "CPU",
+    Preco = 1200,
+    Quantidade = 75
+});
+
+app.MapGet("/produto/listar", (string nome) => 
+{
+    return Results.Ok (produtos);
+});
+
+//GET:/produto/cadastrar
+
+app.MapGet("/produto/cadastrar/{nome}", (string nome) => 
+{
+    Produto produto = new Produto();
+    produto.Nome = nome;
+    produtos.Add(produto);
+    return Results.Ok(produtos);
+});
+
 
 //criar um funcionalidade 
 
+
+
 app.Run();
+
+
+
+// Produto produto = new Produto();
+// produto.Preco = 5;
+// produto.
+// Console.WriteLine("Preco: " + produto.Preco);
+
+// Produto produto = new Produto();
+// produto.setPreco(5);
+// Console.WriteLine("Preco: " + produto.getPreco());
